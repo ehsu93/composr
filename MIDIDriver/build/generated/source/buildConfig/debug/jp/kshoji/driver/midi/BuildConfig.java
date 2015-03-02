31c7830d6f2c22198bb895fa6c060d8f2259d93b
Meaghan <mef79@case.edu>
2015-03-01 22:12:33 -0500

    Migrating project to Android Studio 1.1.0

--- MIDIDriver/build/generated/source/buildConfig/debug/jp/kshoji/driver/midi/BuildConfig.java
+++ MIDIDriver/build/generated/source/buildConfig/debug/jp/kshoji/driver/midi/BuildConfig.java
@@ -5,7 +5,7 @@ package jp.kshoji.driver.midi;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "jp.kshoji.driver.midi";
+  public static final String APPLICATION_ID = "jp.kshoji.driver.midi";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
--- MIDIDriver/build/generated/source/buildConfig/release/jp/kshoji/driver/midi/BuildConfig.java
+++ MIDIDriver/build/generated/source/buildConfig/release/jp/kshoji/driver/midi/BuildConfig.java
@@ -5,7 +5,7 @@ package jp.kshoji.driver.midi;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "jp.kshoji.driver.midi";
+  public static final String APPLICATION_ID = "jp.kshoji.driver.midi";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
